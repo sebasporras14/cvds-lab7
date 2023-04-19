a7832fa10a5bc1ac4acfd046215672966956f9dd
sebasporras14 <sebastian.porras@mail.escuelaing.edu.co>
2023-04-19 12:00:30 -0500

    a

---
+++ src/main/java/eci/edu/cvds/lab07/comporrassebasmodel/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package eci.edu.cvds.lab07.comporrassebasmodel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ src/test/java/eci/edu/cvds/lab07/Lab07ApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.edu.cvds.lab07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
